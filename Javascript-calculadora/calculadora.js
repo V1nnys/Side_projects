b12f23b5ee18570bfc71c561f0ae13067fd22917
Vinicius Silva <vinicius.silva.89@outlook.com>
2021-07-29 09:14:03 -0300

    JS-calculadora02
    *Tem um erro ao usar vírgula para contas decimais.

--- Javascript-calculadora/calculadora.js
+++ Javascript-calculadora/calculadora.js
@@ -8,6 +8,29 @@ let novoNumero = true;
 let operador; //global
 let numeroAnterior; //armazenar valor
 
+const operacaoPendente = () => operador !== undefined;
+
+const calcular = () => {
+  if (operacaoPendente()) {
+    const numeroAtual = parseFloat(display.textContent);
+    novoNumero = true;
+    const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`); //forma resumida
+    atualizarDisplay(resultado);
+
+    /*
+    if (operador == "+") {
+      atualizarDisplay(numeroAnterior + numeroAtual);
+    } else if (operador == "-") {
+      atualizarDisplay(numeroAnterior - numeroAtual);
+    } else if (operador == "*") {
+      atualizarDisplay(numeroAnterior * numeroAtual);
+    } else if (operador == "/") {
+      atualizarDisplay(numeroAnterior / numeroAtual);
+    }
+    */
+  }
+};
+
 const atualizarDisplay = (texto) => {
   if (novoNumero) {
     display.textContent = texto; //limpar
@@ -22,18 +45,73 @@ numeros.forEach((numero) => numero.addEventListener("click", inserirNumero));
 
 //operadores
 const selecionarOperador = (evento) => {
-  novoNumero = true;
-  operador = evento.target.textContent;
-  numeroAnterior = display.textContent;
+  if (!novoNumero) {
+    calcular();
+    novoNumero = true;
+    operador = evento.target.textContent;
+    numeroAnterior = parseFloat(display.textContent);
+    console.log(operador);
+  }
 };
 operadores.forEach((operador) =>
   operador.addEventListener("click", selecionarOperador)
 );
+
+const ativarIgual = () => {
+  calcular();
+  operador = undefined;
+};
+
+const limparDisplay = () => (display.textContent = "");
+document
+  .getElementById("limparDisplay")
+  .addEventListener("click", limparDisplay);
+
+document.getElementById("igual").addEventListener("click", ativarIgual);
+
+const limparCalculo = () => {
+  limparDisplay();
+  operador = undefined;
+  numeroNovo = true;
+  numeroAnterior = undefined;
+};
+
+document
+  .getElementById("limparCalculo")
+  .addEventListener("click", limparCalculo);
+
+const removerUltimonumero = () =>
+  (display.textContent = display.textContent.slice(0, -1)); //Obs 3
+document
+  .getElementById("backspace")
+  .addEventListener("click", removerUltimonumero);
+
+const inverterSinal = () => {
+  novoNumero = true;
+  atualizarDisplay(display.textContent * -1);
+};
+document.getElementById("inverter").addEventListener("click", inverterSinal);
+
+const existeDecimal = () => display.textContent.indexOf(",") !== -1;
+const existeValor = () => display.textContent.lenght > 0;
+const inserirDecimal = () => {
+  if (!existeDecimal()) {
+    if (existeValor()) {
+      atualizarDisplay(",");
+    } else {
+      atualizarDisplay("0,");
+    }
+  }
+};
+document.getElementById("decimal").addEventListener("click", inserirDecimal);
 
 /*
 Obs 1: querySelectorAll("[id*=tecla]"); É criado uma array com os dez valores e a busca é pelo que eles tem em comum,
 a palavra TECLA, usando o * depois do id.
 
 Obs 2: += pra concatenação
+
+Obs 3: Slice() É um método usado em arrays. O textContent retorna uma string e uma string é um array de caracteres.
+Então Slice inicia no 0 e remove o último número/caractere.
 
 */
