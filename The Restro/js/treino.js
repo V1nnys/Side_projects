b8feff60fb37d2c15053c7ccab445fb75609cc9f
Vinicius Silva <vinicius.silva.89@outlook.com>
2021-09-10 08:35:28 -0300

    The-Restro-1.1
    Alguns ajustes na estrutura da página principal.
    
    Adicionada sessão de depoimentos na página de menu. Utilizando javascript para navegação. Ainda não está como o esperado. Buscando a solução.

--- The Restro/js/treino.js
+++ The Restro/js/treino.js
@@ -2,4 +2,28 @@ function showMenu() {
   let menushow = document.getElementById(`m-mobile`);
   window.alert("Menu funcionando");
 }
-// eita porra!!!!
+
+// function swipeRight() {
+//   document.getElementById(`dep-content`).classList.remove("dep-content");
+//   document.getElementById(`dep-content`).classList.add(`swipe`);
+// }
+
+// function swiperLeft() {
+//   document.getElementById(`dep-content`).classList.remove("swipe");
+//   document.getElementById(`dep-content`).classList.add(`dep-content`);
+// }
+
+function swipeRight() {
+  document
+    .getElementById("dep-content")
+    .setAttribute("style", "position:relative;left:-45%;");
+  if (scrollX < 1) {
+    //window.alert("Funciona!");
+  }
+}
+
+function swiperLeft() {
+  document
+    .getElementById("dep-content")
+    .setAttribute("style", "position:relative;left:0;");
+}
