65b01e5698090ddfcea25b41bd9e5d80c7f9179a
Vinicius Silva <vinicius.silva.89@outlook.com>
2021-09-27 08:06:10 -0300

    JS-Urna-02
    Ainda sem funcionalidade, apenas estilo.

---
+++ JS-Urna/etapas.js
@@ -0,0 +1,47 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: "Moe",
+        partido: "ABC",
+        fotos: [{ url: "moe.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: 27555,
+        nome: "Appu",
+        partido: "ABC",
+        fotos: [{ url: "appu.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: "Homer",
+        partido: "SPGF",
+        Vice: "Bart",
+        fotos: [
+          { url: "homer.jpg", legenda: "Prefeito" },
+          { url: "bart.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: 77,
+        nome: "Lisa",
+        partido: "SPGF",
+        Vice: "Marge",
+        fotos: [
+          { url: "lisa.jpg", legenda: "Prefeita" },
+          { url: "marge.jpg", legenda: "Vice-Prefeita", small: true },
+        ],
+      },
+    ],
+  },
+];
